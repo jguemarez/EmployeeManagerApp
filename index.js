b3bf584d78da5d6dcfaf512bf8c4ca351ee62daf
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-09-20 15:43:07 -0400

    11th commit: Defined the function that displays the title inside the asciiTitle.js file of the lib folder and imported it; imported the function from that module into index.js; changed the title of this package to employee-manager-app.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const startCLI = require('./lib/inquire_query.js') ;
 const db = require('./config/connection.js');
+const displayTitle = require('./lib/asciiTitle.js');
 
 db.connect((err) => {
     if (err) throw err;
-    //displayTitle();
+    displayTitle();
     console.info(`
-      Welcome to the Employee Tracker App v 1.0.0. Just select the option that better suites your needs from our intuitive menu and follow the queries to rapidly retrieve and modify info 
+      Welcome to the Employee Manager App v 1.0.0. Just select the option that better suites your needs from our intuitive menu and follow the queries to rapidly retrieve and modify info 
     from our employee database. 
     `);
     startCLI();
